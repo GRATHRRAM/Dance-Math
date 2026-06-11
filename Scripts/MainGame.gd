aa824db39aa3c41389d1558a274fa17a7dda250e
Grah <101575465+GRATHRRAM@users.noreply.github.com>
2026-06-11 08:44:59 +0200

    Refactor And New Mods

---
+++ Scripts/MainGame.gd
@@ -0,0 +1,307 @@
+extends Node2D
+
+enum Direction {
+	LEFT,
+	UP,
+	RIGHT
+}
+
+enum Mode {
+	ADDITION,
+	SUBTRACTION,
+	MULTIPLICATION,
+	DIVISION,
+	SQUAREROOT
+}
+
+var max_gen_number = 3
+
+var reset_time = 6.0
+var times = [reset_time, reset_time]
+
+var points = [0, 0]
+var solutions = [Direction.LEFT, Direction.LEFT]
+
+var half_screen := 0.0
+
+var CorrectScene: PackedScene = preload("res://Scenes/Correct.tscn")
+var WrongScene: PackedScene = preload("res://Scenes/Wrong.tscn")
+var TimeScene: PackedScene = preload("res://Scenes/time.tscn")
+
+@onready var players = [
+	$Gui/Player1,
+	$Gui/Player2
+]
+
+# Two Buttons At the Same time
+var t1 := -1.0
+var t2 := -1.0
+var window := 0.1
+
+
+func _ready() -> void:
+	randomize()
+
+	make_task(0)
+	make_task(1)
+
+	update_points(0)
+	update_points(1)
+
+
+func _process(delta: float) -> void:
+	half_screen = get_viewport_rect().size.x / 2
+
+	if Input.is_action_just_pressed("Reset"):
+		handle_reset()
+	if check_buttons():
+		handle_reset()
+
+	if is_game_over():
+		return
+
+	handle_inputs()
+	update_timers(delta)
+	check_winner()
+
+
+# --------------------------------------------------
+# TASKS
+# --------------------------------------------------
+
+func generate_task(mode : Mode) -> Dictionary:
+	var question := ""
+	var correct_answer := 0
+
+	match mode:
+		0: # Addition
+			var a = randi_range(0, max_gen_number)
+			var b = randi_range(0, max_gen_number)
+			question = "%d + %d" % [a, b]
+			correct_answer = a + b
+
+		1: # Subtraction (no negatives)
+			var a = randi_range(0, max_gen_number)
+			var b = randi_range(0, a)
+			question = "%d - %d" % [a, b]
+			correct_answer = a - b
+
+		2: # Multiplication
+			var a = randi_range(0, max_gen_number)
+			var b = randi_range(0, max_gen_number)
+			question = "%d * %d" % [a, b]
+			correct_answer = a * b
+
+		3: # Division (always clean integer)
+			var answer = randi_range(1, max_gen_number)
+			var divisor = randi_range(1, max_gen_number)
+			var dividend = answer * divisor
+			question = "%d / %d" % [dividend, divisor]
+			correct_answer = answer
+
+		4: # Square root (perfect square)
+			var answer = randi_range(0, max_gen_number)
+			var square = answer * answer
+			question = "√%d" % square
+			correct_answer = answer
+
+	# -----------------------------
+	# Generate safe unique answers
+	# -----------------------------
+	var answers = [correct_answer]
+	var max_offset = max(10, abs(correct_answer))
+
+	while answers.size() < 3:
+		var wrong = correct_answer + randi_range(-max_offset, max_offset)
+
+		if wrong < 0:
+			continue
+
+		if wrong not in answers:
+			answers.append(wrong)
+
+	answers.shuffle()
+
+	return {
+		"equation": question,
+		"answers": answers,
+		"solution": answers.find(correct_answer)
+	}
+
+
+func make_task(player: int) -> void:
+	times[player] = reset_time
+
+	var task = generate_task(Mode.ADDITION)
+
+	solutions[player] = task.solution
+
+	players[player].get_node("eq").text = task.equation
+	players[player].get_node("OptionLeft").text = str(task.answers[0])
+	players[player].get_node("OptionUp").text = str(task.answers[1])
+	players[player].get_node("OptionRight").text = str(task.answers[2])
+
+
+# --------------------------------------------------
+# ANSWERS
+# --------------------------------------------------
+
+func answer(player: int, choice: int) -> void:
+	var correct = choice == solutions[player]
+
+	if correct:
+		points[player] += 1
+		spawn_particle("Correct", random_player_x(player))
+	else:
+		points[player] -= 1
+		spawn_particle("Wrong", random_player_x(player))
+
+	update_points(player)
+	make_task(player)
+
+
+# --------------------------------------------------
+# INPUT
+# --------------------------------------------------
+
+func handle_inputs() -> void:
+	if Input.is_action_just_pressed("Left1"):
+		answer(0, Direction.LEFT)
+
+	if Input.is_action_just_pressed("Up1"):
+		answer(0, Direction.UP)
+
+	if Input.is_action_just_pressed("Right1"):
+		answer(0, Direction.RIGHT)
+
+	if Input.is_action_just_pressed("Left2"):
+		answer(1, Direction.LEFT)
+
+	if Input.is_action_just_pressed("Up2"):
+		answer(1, Direction.UP)
+
+	if Input.is_action_just_pressed("Right2"):
+		answer(1, Direction.RIGHT)
+
+
+# --------------------------------------------------
+# TIMERS
+# --------------------------------------------------
+
+func update_timers(delta: float) -> void:
+	for player in range(2):
+		times[player] -= delta
+
+		if times[player] < 0:
+			points[player] -= 1
+
+			update_points(player)
+			make_task(player)
+
+			spawn_particle(
+				"Time",
+				random_player_x(player)
+			)
+
+		players[player].get_node("Time").text = \
+			"Czas: " + str(snapped(times[player], 1))
+
+
+# --------------------------------------------------
+# UI
+# --------------------------------------------------
+
+func update_points(player: int) -> void:
+	players[player].get_node("PointsInfo").text = \
+		"Punkty: " + str(points[player])
+
+
+func check_winner() -> void:
+	if points[0] > 10:
+		players[0].get_node("Win").visible = true
+
+	if points[1] > 10:
+		players[1].get_node("Win").visible = true
+
+
+func is_game_over() -> bool:
+	return \
+		players[0].get_node("Win").visible or \
+		players[1].get_node("Win").visible
+
+
+func handle_reset() -> void:
+	points[0] = 0
+	points[1] = 0
+
+	players[0].get_node("Win").visible = false
+	players[1].get_node("Win").visible = false
+
+	update_points(0)
+	update_points(1)
+
+	make_task(0)
+	make_task(1)
+
+
+# --------------------------------------------------
+# PARTICLES
+# --------------------------------------------------
+
+func spawn_particle(type: String, x: float) -> void:
+	var obj
+
+	match type:
+		"Correct":
+			obj = CorrectScene.instantiate()
+
+		"Time":
+			obj = TimeScene.instantiate()
+
+		_:
+			obj = WrongScene.instantiate()
+
+	obj.StartPosition = Vector2(
+		x,
+		randi_range(0, 300)
+	)
+
+	obj.scale = Vector2(0.2, 0.2)
+
+	add_child(obj)
+
+
+func random_player_x(player: int) -> float:
+	if player == 0:
+		return randf_range(0, half_screen)
+
+	return randf_range(
+		half_screen,
+		half_screen * 2
+	)
+
+func check_buttons() -> bool:
+	var now = Time.get_ticks_msec() / 1000.0
+
+	var triggered := false
+
+	if Input.is_action_just_pressed("Down1"):
+		t1 = now
+
+	if Input.is_action_just_pressed("Down2"):
+		t2 = now
+
+	if t1 > 0 and t2 > 0:
+		if abs(t1 - t2) <= window:
+			triggered = true
+			t1 = -1
+			t2 = -1
+
+	# expire old inputs (IMPORTANT)
+	if t1 > 0 and now - t1 > window:
+		t1 = -1
+
+	if t2 > 0 and now - t2 > window:
+		t2 = -1
+
+	return triggered
